b114bc9ec86305207825245d0b99a1ba6799addd
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-07 15:25:55 +0000

    Finished hollow astericks box

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Creates a hollow box of astericks
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets number from user
+    System.out.println("Please enter a number between 1 and 10");
+    int userNum = input.nextInt();
+
+
+    //loop for vertical astericks
+    for(int i = 1; i <= userNum; i++){
+      for(int j = 1; j <= userNum; j++){
+        if(i == 1 || i == userNum || j == 1 || j == userNum){
+          System.out.print("*");
+        }else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println("");
+    }
   }
 }
